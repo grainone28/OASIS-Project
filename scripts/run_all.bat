5059b1c2f365a765a314043a27192af4154f7630
Gerardo Rainone <grainone28@gmail.com>
2026-05-12 16:37:34 +0200

    chore: moved test script to tests directory

--- scripts/run_all.bat
+++ scripts/run_all.bat
@@ -1,4 +1,5 @@
 @echo off
+cd ..
 echo ========================================================
 echo   OASIS PROJECT - COMPLETE EVALUATION AUTOMATION
 echo ========================================================
